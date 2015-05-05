276462c7f4c1cbcc50e2512d838ed3750edc1760
Dylan Conrad Johnson <dylanatasmsa@gmail.com>
2015-05-05 14:31:56 -0500

    update

--- codetalks/apps/view/views.py
+++ codetalks/apps/view/views.py
@@ -4,37 +4,12 @@ from django.core.urlresolvers import reverse
 from database.project import interface
 
 def Index(request):
-	# try:
-	return render(request, 'view/index.html')
-	# except:
-	# 	return HttpResponseRedirect(reverse("apps.home.home.Index"))
-#
-# def GetNewProject(request):
-# 	projectHash = interface.GenerateNewProject()
-# 	if projectHash is not None:
-# 		url = reverse('editor:index')
-# 		url += "?projecthash="
-# 		url += projectHash
-# 		return HttpResponseRedirect(url)
-# 	else:
-# 		return HttpResponseRedirect(reverse('apps.home.home.Index'))
-#
-# def Fork(request):
-# 	result = interface.Fork(request.POST['projecthash'])
-# 	returnString = "The code was forked." if result else "An error occured."
-# 	return HttpResponse(returnString)
-#
-# def SaveCode(request):
-# 	result = interface.SaveCode(request.POST['projecthash'], request.POST['code'])
-# 	returnString = "The code was saved." if result else "An error occured."
-# 	return HttpResponse(returnString)
-#
-# def AlterPublicPrivate(request):
-# 	result = interface.AlterPublicPrivate(request.POST['projecthash'])
-# 	returnString = "Make Private" if result else "Make Public"
-# 	return HttpResponse(returnString)
-#
-# def AlterName(request):
-# 	result = interface.ChangeName(request.POST['projecthash'], request.POST['name'])
-# 	returnString = "The name was changed." if result is True else "An error occured."
-# 	return HttpResponse(returnString)
+	try:
+		pres = interface.GetPres(request.GET['projecthash'])
+		markdown = pres.presMarkdown.split("<break>")
+		htmldata = {
+			'markdownlist':markdown
+		}
+		return render(request, 'view/index.html', htmldata)
+	except:
+		return HttpResponseRedirect(reverse("apps.home.home.Index"))
